195ed60a4aa1ee1afa16eb7f9c958979c8baa389
Rushabh Sancheti <rushabhsancheti2397@gmail.com>
2025-07-22 13:59:46 +0530

    feat: enable rbac

--- src/main/java/com/rushabh/ecommerce/security/UserPrincipal.java
+++ src/main/java/com/rushabh/ecommerce/security/UserPrincipal.java
@@ -2,10 +2,12 @@ package com.rushabh.ecommerce.security;
 
 import com.rushabh.ecommerce.entity.User;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class UserPrincipal implements UserDetails {
 
@@ -21,7 +23,9 @@ public class UserPrincipal implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+         return user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getName()))
+                .collect(Collectors.toSet());
     }
 
     @Override
